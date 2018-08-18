75ee974b4187d7d4492a1f1e438601970bc8f0cd
Yen Nguyen <ngocyen300693@gmail.com>
2018-08-18 14:46:38 +0700

    Remove unused code

--- hackerrank/FindString.py
+++ hackerrank/FindString.py
@@ -3,12 +3,6 @@
 
 def count_substring(string, sub_string):
     count = 0
-    len_sub = len(sub_string)
-    # if (len_sub == 1):
-    #     for i in range(0, len(string)):
-    #         if string[i] == sub_string:
-    #             count = count + 1
-    #     return count
     for i in range(0, len(string)):
         if(string[i:len(sub_string)+i] == sub_string):
             count = count + 1
