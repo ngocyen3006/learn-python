8e4c949fd7537cf0d7073464393775149af45e50
Yen Nguyen <ngocyen300693@gmail.com>
2018-08-25 20:55:01 +0700

    Solve "Text Wrap" on hackerrank

---
+++ hackerrank/textWrap.py
@@ -0,0 +1,14 @@
+# https://www.hackerrank.com/challenges/text-wrap/problem
+
+import textwrap
+
+
+def wrap(string, max_width):
+    alist = textwrap.wrap(string, max_width)
+    return "\n".join(alist)
+
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
