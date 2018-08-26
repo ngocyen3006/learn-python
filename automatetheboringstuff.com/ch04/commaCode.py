7fe1543941ac1e6b56dc39d83a8450ae011a312b
Yen Nguyen <ngocyen300693@gmail.com>
2018-08-26 20:31:50 +0700

    Fix bug in "Comma Code"

--- automatetheboringstuff.com/ch04/commaCode.py
+++ automatetheboringstuff.com/ch04/commaCode.py
@@ -4,13 +4,15 @@ import copy
 def comma(arr):
     if arr == None or arr == []:
         return None
+    elif len(arr) == 1:
+        return arr[0]
     else:
         temp = copy.copy(arr)
         del temp[-1]
         string = "'" + ", ".join(temp) + ", and " + arr[-1] + "'"
-    return string
+        return string
 
 
-spam = ['apples', 'bananas', 'tofu', 'cats']
+spam = ['apples', 'bananas', 'tofu', 'cats', 'dogs']
 result = comma(spam)
 print(result)
