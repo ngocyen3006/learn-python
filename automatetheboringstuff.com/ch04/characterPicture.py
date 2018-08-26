49ec6ea1240cac469f9ced13fcde7080e0688c13
Yen Nguyen <ngocyen300693@gmail.com>
2018-08-26 23:34:03 +0700

    Change code

--- automatetheboringstuff.com/ch04/characterPicture.py
+++ automatetheboringstuff.com/ch04/characterPicture.py
@@ -8,7 +8,10 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
 
-for i in range(len(grid[0])):
-    print(end="\n")
-    for j in range(0, len(grid)):
-        print(grid[j][i], end='')
+# for i in range(len(grid[0])):
+#     print(end="\n")
+#     for j in range(0, len(grid)):
+#         print(grid[j][i], end='')
+
+for whatever in (zip(*grid)):
+    print(''.join(whatever))
