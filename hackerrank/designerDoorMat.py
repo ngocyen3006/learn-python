5f68cf2d43be293defbe3431b34900744962605b
Yen Nguyen <ngocyen300693@gmail.com>
2018-08-26 17:15:19 +0700

    Improve previous code

--- hackerrank/designerDoorMat.py
+++ hackerrank/designerDoorMat.py
@@ -1,20 +1,16 @@
 # https://www.hackerrank.com/challenges/designer-door-mat/problem
 
-s = list(map(int, input().split()))
-n = s[0]
-m = s[1]
+n, m = map(int, input().split())
 
 char = ".|."
 
 # top door
-for i in range(0, n // 2):
-    print((char * i).rjust(m // 2 - 1, "-") +
-          char + (char * i).ljust(m // 2 - 1, "-"))
+for i in range(1, n, 2):
+    print((char * i).center(m, "-"))
 
 # center line
 print('WELCOME'.center(m, "-"))
 
 # bottom door
-for i in range(0, n // 2):
-    print((char * ((n // 2) - i - 1)).rjust(m // 2 - 1, "-") +
-          char + (char * ((n // 2) - i - 1)).ljust(m // 2 - 1, "-"))
+for i in range(1, n, 2):
+    print((char * (n - i - 1)).center(m, "-"))
