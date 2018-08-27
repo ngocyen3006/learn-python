634b827591e604ce274ffc240778301db6a97540
Yen Nguyen <ngocyen300693@gmail.com>
2018-08-27 17:36:57 +0700

    Example "get" method

---
+++ automatetheboringstuff.com/ch05/getMethod.py
@@ -0,0 +1,6 @@
+# The get() method
+
+picnicItems = {'apples': 5, "cups": 2}
+print('I am bringing ' + str(picnicItems.get('cups', 0)) + ' cups.')
+
+print('I am bringing ' + str(picnicItems.get('eggs', 0)) + ' eggs.')
