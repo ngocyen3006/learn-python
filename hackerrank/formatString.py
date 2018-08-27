d06d821d0470b311b77bc2a8b26832dd9863320a
Yen Nguyen <ngocyen300693@gmail.com>
2018-08-27 14:39:15 +0700

    Solve "String Formatting" on hackerrank

---
+++ hackerrank/formatString.py
@@ -0,0 +1,18 @@
+# https://www.hackerrank.com/challenges/python-string-formatting/problem
+
+n = int(input())
+
+
+def print_formatted(number):
+    width = len('{0:b}'.format(number))
+
+    for i in range(1, number + 1):
+        # decNum = i  # decimal i
+        # octNum = oct(i)[2:]  # octal i
+        # hexNum = hex(i)[2:]  # hexadecimal i
+        # binNum = bin(i)[2:]  # binary i
+        print(
+            "{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width=width))
+
+
+result = print_formatted(n)
