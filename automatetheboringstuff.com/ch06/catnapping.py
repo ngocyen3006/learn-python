92d8e42917b5bde23f62b6ae4aaf759e14fdf5cd
Yen <ngocyen300693@gmail.com>
2018-09-21 16:35:34 +0700

    Working with Strings

---
+++ automatetheboringstuff.com/ch06/catnapping.py
@@ -0,0 +1,18 @@
+# An example about Triple Quotes
+
+print('''Dear Alice,
+
+Eve's cat has been arrested for catnapping, cat burglary, and extortion.
+
+Sincerely,
+Bob''')
+
+
+# An example about Multiline comments
+def spam():
+    """This is a multiline comment to help
+    explain what the spam() function does."""
+    print('Hello!')
+
+
+spam()
