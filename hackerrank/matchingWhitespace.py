8761c380e971614e16283f08e7a3220a2561fafa
Yen <ngocyen300693@gmail.com>
2018-09-25 23:07:15 +0700

    Solve "Matching Whitespace & Non-Whitespace Character" on hackerrank.

---
+++ hackerrank/matchingWhitespace.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/matching-whitespace-non-whitespace-character/problem
+
+Regex_Pattern = r"^\S{2,}\s\S{2}\s\S{2,}$"  # Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
