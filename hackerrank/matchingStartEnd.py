45fbd8062a045a7f2e70cf771da672d9fa3c6c88
Yen <ngocyen300693@gmail.com>
2018-09-26 11:28:17 +0700

    Solve "Matching start and end" on hackerrank.

---
+++ hackerrank/matchingStartEnd.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/matching-start-end/problem
+
+Regex_Pattern = r"^\d\w{4}.$"  # Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
