eafd21105ef29aeb0490d652fa02ff09aa75f402
Yen <ngocyen300693@gmail.com>
2018-09-28 13:06:18 +0700

    Solve "Matching specific character" on hackerrank.

---
+++ hackerrank/matchingSpecificCharater.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/matching-specific-characters/problem
+
+Regex_Pattern = r'^[1-3][0-2][xs0][30Aa][xsu][\.|,]$'  # Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
