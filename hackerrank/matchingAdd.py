83f721042ba2321e06d69e9bccf688c86cf8fce1
Yen <ngocyen300693@gmail.com>
2018-09-28 21:37:44 +0700

    Solve "Matching one or more repetitions" on hackerrank.

---
+++ hackerrank/matchingAdd.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/matching-one-or-more-repititions/problem
+
+Regex_Pattern = r'^(\d)+[A-Z]+[a-z]+$'  # Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
