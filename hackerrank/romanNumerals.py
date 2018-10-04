d8a2ad508f19dcc614561fa8ee1f1076bc7ae500
Yen Nguyen <ngocyen300693@gmail.com>
2018-10-04 21:42:55 +0700

    Edit comment.

--- hackerrank/romanNumerals.py
+++ hackerrank/romanNumerals.py
@@ -5,9 +5,9 @@ def checkRomanNumerals(s):
     if not isinstance(s, str):
         return False
     # thousands = r"M{,3}"
-    # hundreds = r"[C{1,3}|C[DM]|DC{,3}]?"
-    # tens = r"[X{1,3}|X[LC]|LX{,3}]?"
-    # digits = r"[I{1,3}|I[VX]|VI{,3}]?"
+    # hundreds = r"C{1,3}|C[DM]|DC{,3}"
+    # tens = r"X{1,3}|X[LC]|LX{,3}]"
+    # digits = r"I{1,3}|I[VX]|VI{,3}"
     romanNum = r"^(M{,3})(C[DM]|D?C{,3})(X[LC]|L?X{,3})(I[VX]|V?I{,3})$"
     return bool(re.match(romanNum, s))
 
