67dfbe0e0ad55ed322871219b100ca9b16409d28
Yen Nguyen <ngocyen300693@gmail.com>
2018-10-05 21:50:24 +0700

    Solve "List overlap" on practicepython.org

---
+++ practicepython.org/listOverlap.py
@@ -0,0 +1,23 @@
+def sortArray(a):
+    b = []
+    for i in range(len(a)):
+        if a == []:
+            return b
+        b.append(min(a))
+        a.remove(min(a))
+        for k in b:
+            while a != [] and k == min(a):
+                a.remove(min(a))
+    return b
+
+
+def overlap(a, b):
+    a1 = sortArray(a)
+    b1 = sortArray(b)
+    c = a1 + b1
+    return sortArray(c)
+
+
+a = [1, 5, 3, 4, 1, 6, 2, 1]
+b = [9, 10, 8, 5, 7, 6, 2, 11, 14, 2, 2, 3, 4, 9, 8]
+print(overlap(a, b))
