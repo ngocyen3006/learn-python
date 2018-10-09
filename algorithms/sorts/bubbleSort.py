8af0bd3a93c4f10d02802d4844f4d7f920a4be3b
Yen Nguyen <ngocyen300693@gmail.com>
2018-10-09 20:26:38 +0700

    Change function name.

--- algorithms/sorts/bubbleSort.py
+++ algorithms/sorts/bubbleSort.py
@@ -1,4 +1,4 @@
-def sort(arr):
+def bubbleSort(arr):
     for j in range(1,len(arr)):
         for i in range(len(arr) - j):
             if arr[i] > arr[i + 1]:
--- algorithms/sorts/test_bubbleSort.py
+++ algorithms/sorts/test_bubbleSort.py
@@ -1,12 +1,12 @@
 import unittest
-from bubbleSort import sort
+from bubbleSort import bubbleSort
 
 
 class TestBubbleSort(unittest.TestCase):
     def test_sort(self):
-        self.assertEqual(sort([2, 3, 1, 6, 7, 5, 4, 2]), [1, 2, 2, 3, 4, 5, 6, 7])
-        self.assertEqual(sort([]), [])
-        self.assertEqual(sort([0, 1, 5, 3, 1, 2, 1]), [0, 1, 1, 1, 2, 3, 5])
+        self.assertEqual(bubbleSort([2, 3, 1, 6, 7, 5, 4, 2]), [1, 2, 2, 3, 4, 5, 6, 7])
+        self.assertEqual(bubbleSort([]), [])
+        self.assertEqual(bubbleSort([0, 1, 5, 3, 1, 2, 1]), [0, 1, 1, 1, 2, 3, 5])
 
 
 if __name__ == '__main__':
