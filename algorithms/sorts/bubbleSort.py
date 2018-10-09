860683ad6dec74d65312e783ca8c82f9957f4f0d
Yen Nguyen <ngocyen300693@gmail.com>
2018-10-09 20:41:48 +0700

    Edit code.

--- algorithms/sorts/bubbleSort.py
+++ algorithms/sorts/bubbleSort.py
@@ -1,10 +1,7 @@
 def bubbleSort(arr):
-    for j in range(1,len(arr)):
+    for j in range(1, len(arr)):
         for i in range(len(arr) - j):
             if arr[i] > arr[i + 1]:
                 t = arr[i]
                 arr[i] = arr[i + 1]
                 arr[i + 1] = t
-            else:
-                continue
-    return arr
--- algorithms/sorts/test_bubbleSort.py
+++ algorithms/sorts/test_bubbleSort.py
@@ -4,9 +4,15 @@ from bubbleSort import bubbleSort
 
 class TestBubbleSort(unittest.TestCase):
     def test_sort(self):
-        self.assertEqual(bubbleSort([2, 3, 1, 6, 7, 5, 4, 2]), [1, 2, 2, 3, 4, 5, 6, 7])
-        self.assertEqual(bubbleSort([]), [])
-        self.assertEqual(bubbleSort([0, 1, 5, 3, 1, 2, 1]), [0, 1, 1, 1, 2, 3, 5])
+        a = [2, 3, 1, 6, 7, 5, 4]
+        b = []
+        c = [0, 1, 5, 3, 1, 2, 1]
+        bubbleSort(a)
+        bubbleSort(b)
+        bubbleSort(c)
+        self.assertEqual(a, [1, 2, 3, 4, 5, 6, 7])
+        self.assertEqual(b, [])
+        self.assertEqual(c, [0, 1, 1, 1, 2, 3, 5])
 
 
 if __name__ == '__main__':
