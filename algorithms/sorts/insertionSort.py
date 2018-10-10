6278452c2357d89989ee77c308cf58769c696ca7
Yen Nguyen <ngocyen300693@gmail.com>
2018-10-10 14:05:10 +0700

    Do Insertion Sort.

---
+++ algorithms/sorts/insertionSort.py
@@ -0,0 +1,8 @@
+def insertionSort(a):
+    for i in range(1, len(a)):
+        x = a[i]
+        j = i - 1
+        while j >= 0 and a[j] > x:
+            a[j + 1] = a[j]
+            j = j - 1
+        a[j + 1] = x
---
+++ algorithms/sorts/test_insertionSort.py
@@ -0,0 +1,19 @@
+import unittest
+from insertionSort import insertionSort
+
+
+class TestInsertionSort(unittest.TestCase):
+    def test_sort(self):
+        a = [2, 3, 1, 6, 7, 5, 4]
+        b = []
+        c = [0, 1, 5, 3, 1, 2, 1]
+        insertionSort(a)
+        insertionSort(b)
+        insertionSort(c)
+        self.assertEqual(a, [1, 2, 3, 4, 5, 6, 7])
+        self.assertEqual(b, [])
+        self.assertEqual(c, [0, 1, 1, 1, 2, 3, 5])
+
+
+if __name__ == '__main__':
+    unittest.main()
