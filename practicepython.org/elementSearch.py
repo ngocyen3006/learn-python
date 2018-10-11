cd5ec609b4573dd379e9ec238dee3d84bf4b54e9
Yen Nguyen <ngocyen300693@gmail.com>
2018-10-11 23:05:03 +0700

    Improve code

--- practicepython.org/elementSearch.py
+++ practicepython.org/elementSearch.py
@@ -1,4 +1,7 @@
 def find1(arr, number):
+    """
+    Find 1 will search number in the arr by compare it will each single number in arr
+    """
     for i in arr:
         if i == number:
             return True
@@ -6,35 +9,39 @@ def find1(arr, number):
 
 
 def find2(arr, number):
+    '''
+    Use "in" operator to find number in list arr.
+    '''
     if number in arr:
         return True
     return False
 
 
-# use binary search (only ordered list)
 def binarySearch(arr, number):
+    '''
+    Use binary search number in list arr (only ordered list).
+
+    :return: True if number is found and False if not found
+    '''
     if arr == [] or arr == None:
         return False
-    if len(arr) == 1:
-        if number == arr[0]:
-            return True
-    else:
-        mid = len(arr) // 2
-        if number == arr[mid]:
-            return True
-        elif number < arr[mid]:
-            return binarySearch(arr[:mid], number)
-        else:
-            return binarySearch(arr[mid:], number)
-    return False
+
+    mid = len(arr) // 2
+    if number == arr[mid]:
+        return True
+
+    if number < arr[mid]:
+        return binarySearch(arr[:mid], number)
+
+    return binarySearch(arr[mid + 1:], number)
 
 
 import random
 
 if __name__ == "__main__":
-    l = sorted(random.sample(range(50), 10))
-    print(l)
+    arr = sorted(random.sample(range(50), 10))
+    print(arr)
     a = [5, 10, -1, 8, 17, 30, 27]
     for i in a:
         print(i)
-        print(str(find1(l, i)) + " == " + str(find2(l, i)) + " == " + str(binarySearch(l, i)))
+        print("{} == {} == {}".format(find1(arr, i), find2(arr, i), binarySearch(arr, i)))
