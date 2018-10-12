0f92a662fffd7a7af9aaba3842ea66a534700b40
Yen Nguyen <ngocyen300693@gmail.com>
2018-10-12 15:10:10 +0700

    Fix bug quickSort

--- algorithms/sorts/quickSort.py
+++ algorithms/sorts/quickSort.py
@@ -10,42 +10,37 @@ def swap(a, i, j):
 def sort(a, first, last):
     i = first
     j = last
-    if i >= j:  # sort mang trong hoac mang co 1 phan tu
+    if i >= j:  # check len(arr) <= 1
         return
 
-    if j == i + 1:  # so sanh 2 vi tri lien ke sort(a,i,i+1)
+    if j == i + 1:  # check len(arr) = 2
         if a[i] > a[j]:
             swap(a, i, j)
         return
 
-    # sort mang co nhieu hon 2 phan tu
-    pivotIndex = random.randint(first, last)
-    pivot = a[pivotIndex]  # pivot la so ngau nhien thuoc mang a
-    while i < j:
-        # nhung phan tu be hon pivot dua ve ben trai mang
+    # len(arr) > 2
+    i += 1
+    pivotIndex = random.randint(first, last)  # pivot is random number
+    pivot = a[pivotIndex]
+
+    swap(a, first, pivotIndex)
+    while i <= j:
+        # arr[element] < pivot
         while i <= last and a[i] < pivot:
-            i = i + 1
-        # phan tu lon hon hoac bang pivot dua ve ben phai mang
-        while j > i and a[j] >= pivot:
-            j = j - 1
+            i += 1
+        # arr[element] > pivot
+        while j >= i and a[j] >= pivot:
+            j -= 1
         if i < j:
             swap(a, i, j)
-            i = i + 1
-            j = j - 1
+            i += 1
+            j -= 1
+    swap(a, j, first)
 
-    # all element in this sub array are the same
-    if j == -1 or i == last + 1:
-        return
-
-    sort(a, first, j)
+    sort(a, first, j - 1)
     sort(a, i, last)
 
 
 def quickSort(a):
     sort(a, 0, len(a) - 1)
     pass
-
-
-# a = [28, 58, 26, 28, 59, 68, 90, 95]
-# quickSort(a)
-# print(a)
