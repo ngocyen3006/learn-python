afafee424c45123b56279b06e2dca72dcd548e20
Yen Nguyen <ngocyen300693@gmail.com>
2018-10-12 20:30:19 +0700

    Solve "Birthday dictionaries" in practicepython.org

---
+++ practicepython.org/birthdayDict.py
@@ -0,0 +1,13 @@
+birthdays = {"May": "May 01",
+             "June": "June 30",
+             "August": "August 14",
+             "September": "Sep 29"}
+
+print("Welcome to the birthday dictionary. I know the birthdays of:")
+print("\n".join(birthdays.keys()))
+print("Who's birthday do you want to look up?")
+name = input()
+if name not in birthdays.keys():
+    print("I do not have birthday information for {}.".format(name))
+else:
+    print("{}\'s birthday is {}.".format(name, birthdays[name]))
