768df548117ee5337f194bd1776e1cc8b517ce6c
Yen Nguyen <ngocyen300693@gmail.com>
2018-10-13 18:43:30 +0700

    Improve radix sort code

--- algorithms/sorts/radixSort.py
+++ algorithms/sorts/radixSort.py
@@ -1,26 +1,20 @@
-from copy import copy
-
-
 def radixSort(arr):
     if len(arr) < 2:
         return
     maxArr = max(arr)
     digit = len(str(maxArr))
+    temp = arr
     j = 0
     while j < digit:
         count = {0: [], 1: [], 2: [], 3: [], 4: [],
                  5: [], 6: [], 7: [], 8: [], 9: []}
-        for i in arr:
+        for i in temp:
             number = i // (10 ** j)
             modulo = number % 10
             count[modulo].append(i)
-        arr = []
+        temp = []
         for k in range(10):
-            arr = arr + count[k]
+            temp = temp + count[k]
         j += 1
-    return arr
-
-
-a = [2, 3, 1, 6, 7, 5, 4, 8, 9, 10, 15, 14, 13, 12, 11]
-print(radixSort(a))
-# print(a)
+    for i in range(len(arr)):
+        arr[i] = temp[i]
