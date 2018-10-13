61e3e60392a945a8cdafdf54e69edbacf10bb0c2
Yen Nguyen <ngocyen300693@gmail.com>
2018-10-13 09:50:18 +0700

    Do counting sort.

---
+++ algorithms/sorts/countingSort.py
@@ -0,0 +1,21 @@
+def countingSort(arr):
+    count = {}
+    for number in arr:
+        count.setdefault(number, 0)
+        count[number] = count[number] + 1
+        minArr = min(arr)
+        maxArr = max(arr)
+    j = 0
+    while j < len(arr):
+        for i in range(minArr, maxArr + 1):
+            if i in count.keys():
+                if count[i] > 1:
+                    k = 0
+                    while k < count[i] and j + k < len(arr):
+                        arr[j + k] = i
+                        k += 1
+                    j = j + k
+                else:
+                    arr[j] = i
+                    j = j + 1
+    pass
