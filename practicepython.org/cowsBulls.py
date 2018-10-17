a56bffcddb3fa481cec15d61f1ac303ee4e69d5f
Yen Nguyen <ngocyen300693@gmail.com>
2018-10-17 22:59:35 +0700

    Improve code

--- practicepython.org/cowsBulls.py
+++ practicepython.org/cowsBulls.py
@@ -21,21 +21,22 @@ if __name__ == "__main__":
     print(number)
 
     count = 0  # count guess times
-    loop = True
-    while loop:
+    while True:
         # user guess a number
         print("Give me your guess!")
         guess = input()
         count += 1
         if guess.lower() == "exit":
-            loop = False
-        elif len(guess) > len(number):
+            break
+
+        if len(guess) > len(number):
             print("Your guess number is  too large, try again.")
-        else:
-            result = cowBull(number, guess)
-            if result[1] == len(number):
-                print("You win the game with {} bulls after {} guess times.".format(result[1], count))
-                loop = False
-            else:
-                print("You have {} cows, and {} bulls.".format(result[0], result[1]))
-                print("Your guess isn't quite right, try again.")
+            continue
+
+        result = cowBull(number, guess)
+        if result[1] == len(number):
+            print("You win the game with {} bulls after {} guess times.".format(result[1], count))
+            break
+
+        print("You have {} cows, and {} bulls.".format(result[0], result[1]))
+        print("Your guess isn't quite right, try again.")
