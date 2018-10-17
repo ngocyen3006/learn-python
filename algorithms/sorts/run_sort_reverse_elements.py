8b0d6ab53e19d549cd4b6ea9d983772f84d2e5ea
Yen Nguyen <ngocyen300693@gmail.com>
2018-10-17 14:39:13 +0700

    Do file test sort functions with reverse sorted elements

---
+++ algorithms/sorts/run_sort_reverse_elements.py
@@ -0,0 +1,46 @@
+from random import randint
+from bubbleSort import bubbleSort
+from countingSort import countingSort
+from insertionSort import insertionSort
+from mergeSort import mergeSort
+from quickSort import quickSort
+from radixSort import radixSort
+from selectionSort import selectionSort
+import time
+
+lowerbound = -2 ** 31
+upperbound = 2 ** 31
+
+
+def gen(n):
+    '''
+    Generate reverse sorted array random number with n element
+    '''
+    arr = [randint(lowerbound, upperbound) for i in range(n)]
+    reverseSortedArr = sorted(arr)[::-1]
+    return reverseSortedArr
+
+
+# nTests = [100, 1000, 10000]
+nTests = map(int, [1e5, 1e6, 1e7])
+
+# sortFunctions = [bubbleSort, selectionSort, insertionSort,
+#                  quickSort, mergeSort, countingSort, radixSort]
+
+sortFunctions = [mergeSort, countingSort, radixSort]
+timeSort = {"bubbleSort": [], "selectionSort": [], "insertionSort": [],
+            "quickSort": [], "mergeSort": [], "countingSort": [], "radixSort": []}
+for n in nTests:
+    print("----------------------------------------")
+    print(f"Number of element: {n}")
+    print("----------------------------------------")
+    arr = gen(n)
+    for i in sortFunctions:
+        print(f"Testing sort method: {i.__name__}")
+        temp = arr
+        start = time.time()
+        i(temp)
+        end = time.time()
+        run = round((end - start) * 1000, 4)
+        timeSort[i.__name__].append(run)
+    print(timeSort)
