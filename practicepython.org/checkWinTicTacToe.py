0ce776a813ffc765798329e1daef473b6b394c85
Yen Nguyen <ngocyen300693@gmail.com>
2018-10-18 20:57:59 +0700

    Solve "Check tic tac toe" on practicepython.org

--- practicepython.org/checkWinTicTacToe.py
+++ practicepython.org/checkWinTicTacToe.py
@@ -1,13 +1,11 @@
 def checkWinGame(a):
     # check win is a row
     for j in range(3):
-        print(f"j is {j}")
         if a[j][0] == a[j][1] == a[j][2] and a[j][0] != 0:
             return a[j][0]
 
     # check win is a column
     for i in range(3):
-        print(f"i is {i}")
         if a[0][i] == a[1][i] == a[2][i] and a[0][i] != 0:
             return a[0][i]
 
