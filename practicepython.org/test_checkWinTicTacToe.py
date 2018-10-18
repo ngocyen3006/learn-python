898ed1bddee21e97443fc1ce3d0835262d82de34
Yen Nguyen <ngocyen300693@gmail.com>
2018-10-18 20:39:41 +0700

    Do test "Check tic tac toe" on practicepython.org

--- practicepython.org/test_checkWinTicTacToe.py
+++ practicepython.org/test_checkWinTicTacToe.py
@@ -4,45 +4,51 @@ from checkWinTicTacToe import checkWinGame
 
 class TestCheckWinGame(unittest.TestCase):
     def test_winner_is_1(self):
-        round1 = [[1, 1, 0],
-                  [1, 2, 0],
-                  [1, 2, 2]]
-        round2 = [[1, 2, 0],
-                  [2, 1, 0],
-                  [2, 1, 1]]
-        round3 = [[0, 1, 0],
-                  [2, 1, 0],
-                  [2, 1, 1]]
+        test_case = {
+            "round1": [[1, 1, 0],
+                       [1, 2, 0],
+                       [1, 2, 2]],
+            "round2": [[1, 2, 0],
+                       [2, 1, 0],
+                       [2, 1, 1]],
+            "round3": [[0, 1, 0],
+                       [2, 1, 0],
+                       [2, 1, 1]]}
         for i in range(1, 4):
-            arr = "round" + str(i)
+            arr_name = "round" + str(i)
+            arr = test_case[arr_name]
             self.assertEqual(checkWinGame(arr), 1)
 
     def test_winner_is_2(self):
-        round1 = [[2, 2, 0],
-                  [2, 1, 0],
-                  [2, 1, 1]]
-        round2 = [[2, 2, 2],
-                  [1, 1, 0],
-                  [1, 0, 1]]
-        round3 = [[1, 1, 2],
-                  [0, 0, 2],
-                  [1, 1, 2]]
-        round4 = [[0, 1, 2],
-                  [1, 2, 1],
-                  [2, 2, 0]]
+        test_case = {
+            "round1": [[2, 2, 0],
+                       [2, 1, 0],
+                       [2, 1, 1]],
+            "round2": [[2, 2, 2],
+                       [1, 1, 0],
+                       [1, 0, 1]],
+            "round3": [[1, 1, 2],
+                       [0, 0, 2],
+                       [1, 1, 2]],
+            "round4": [[0, 1, 2],
+                       [1, 2, 1],
+                       [2, 2, 0]]}
         for i in range(1, 5):
-            arr = "round" + str(i)
+            arr_name = "round" + str(i)
+            arr = test_case[arr_name]
             self.assertEqual(checkWinGame(arr), 2)
 
     def test_no_winner(self):
-        round1 = [[1, 2, 0],
-                  [2, 1, 0],
-                  [2, 1, 2]]
-        round2 = [[1, 2, 0],
-                  [2, 1, 0],
-                  [2, 1, 0]]
+        test_case = {
+            "round1": [[1, 2, 0],
+                       [2, 1, 0],
+                       [2, 1, 2]],
+            "round2": [[1, 2, 0],
+                       [2, 1, 0],
+                       [2, 1, 0]]}
         for i in range(1, 3):
-            arr = "round" + str(i)
+            arr_name = "round" + str(i)
+            arr = test_case[arr_name]
             self.assertEqual(checkWinGame(arr), 0)
 
 
