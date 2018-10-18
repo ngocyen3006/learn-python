c87b008a421588b4b777ac5f76dcc887acaf79d6
Yen Nguyen <ngocyen300693@gmail.com>
2018-10-18 14:40:50 +0700

    Solve "Guessing game two" on practicepython.org

---
+++ practicepython.org/guessGameTwo.py
@@ -0,0 +1,42 @@
+import random
+
+
+def guessGame():
+    low = 0
+    high = 100
+    count = 0
+
+    # r = int((low + high) // 2)
+    r = random.randint(low, high)
+    print(f"I guess the number you think is: {r}")
+
+    print("Is it True, too low, or too high? ")
+    result = input()
+
+    while result != "true" and result != "exit":
+        while result.lower() == "too low":
+            low = r + 1
+            # r = int((low + high) // 2)
+            r = random.randint(low, high)
+            print(f"I guess the number is: {r}")
+            result = input()
+            count += 1
+
+        while result.lower() == "too high":
+            high = r - 1
+            # r = int((low + high) // 2)
+            r = random.randint(low, high)
+            print(f"I guess the number is: {r}")
+            result = input()
+            count += 1
+
+    if result.lower() == "exit":
+        return
+
+    if result.lower() == "true":
+        print(f"This number is {r} with {count} guesses.")
+        return
+
+
+if __name__ == '__main__':
+    guessGame()
