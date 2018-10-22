978462c2e14cb1ff30b8d2cabdae384a7625bc3c
Yen Nguyen <ngocyen300693@gmail.com>
2018-10-22 13:17:20 +0700

    Improve code "Check strict superset" on hackerrank

--- hackerrank/checkStrictSuperset.py
+++ hackerrank/checkStrictSuperset.py
@@ -1,12 +1,11 @@
 # https://www.hackerrank.com/challenges/py-check-strict-superset/problem
 
-A = list(map(int, input().split()))
-Aset = set(A)
+A = set(map(int, input().split()))
 n = int(input())
 result = []
 for i in range(n):
     subset = set(map(int, input().split()))
-    if subset.intersection(Aset) == subset and len(subset) < len(A):
+    if A > subset:
         result.append(True)
     else:
         result.append(False)
