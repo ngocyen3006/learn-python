672a000233909edbc1bb9c3b01b44ac54f4899e8
Yen Nguyen <ngocyen300693@gmail.com>
2018-10-22 23:19:06 +0700

    Example for class lesson on guru.com

---
+++ class/guru99.py
@@ -0,0 +1,18 @@
+# Example file for working with classes
+class myClass():
+    def method1(self):
+        print("Guru99")
+
+    def method2(self, someString):
+        print("Software Testing:" + someString)
+
+
+def main():
+    # exercise the class methods
+    c = myClass()
+    c.method1()
+    c.method2(" Testing is fun")
+
+
+if __name__ == "__main__":
+    main()
