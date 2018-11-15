baf59234036812839e019b91a75d3f933790dcae
Yen Nguyen <ngocyen300693@gmail.com>
2018-11-15 12:14:47 +0700

    Compare list using "append" method and "+" method

---
+++ thinkpython.com/runtimeFunction.py
@@ -0,0 +1,12 @@
+import time
+
+
+def timed(f):
+    def _timed(*args, **kwargs):
+        start = time.time()
+        result = f(*args, **kwargs)
+        t = (time.time() - start) * 1000
+        print("runtime for function {}: {:0.4f} ms".format(f.__name__, t))
+        return result
+
+    return _timed
