a4aea7722b30f5ef637713964cf383bdca052682
Yen Nguyen <ngocyen300693@gmail.com>
2018-11-17 00:20:50 +0700

    Improve code

--- thinkpython.com/chap 15/point.py
+++ thinkpython.com/chap 15/point.py
@@ -5,11 +5,9 @@ class Point:
     ''' Represents a point in 2-D space.'''
 
 
-def distance(x, y):
-    return math.sqrt(x ** 2 + y ** 2)
-
-
-blank = Point()
-blank.x = 3.0
-blank.y = 4.0
-print(distance(blank.x, blank.y))
+def distance(p1, p2):
+    ''' Computes the distance between two Point objects'''
+    dx = p1.x - p2.x
+    dy = p1.y - p2.y
+    d = math.sqrt(dx ** 2 + dy ** 2)
+    return d
