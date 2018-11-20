c4f779bd513dff71f0e9da5bf5a64426b95ed086
Yen Nguyen <ngocyen300693@gmail.com>
2018-11-20 11:53:25 +0700

    Exercise class Kangaroo solution

---
+++ thinkpython.com/chap 17/kangaroo.py
@@ -0,0 +1,39 @@
+class Kangaroo:
+    '''A Kangaroo is a marsupial.'''
+
+    def __init__(self, name, contents=None):
+        '''
+        Initialize the pouch contents.
+        name: string
+        contents: initial pouch contents.
+        '''
+        self.name = name
+        if contents == None:
+            contents = []
+        self.pouch_contents = contents
+
+    def __str__(self):
+        '''Return a string representaion of this Kangaroo.'''
+
+        t = [self.name + ' has pouch contents:']
+        for obj in self.pouch_contents:
+            s = '    ' + object.__str__(obj)
+            t.append(s)
+        return '\n'.join(t)
+
+    def put_in_pouch(self, item):
+        '''
+        Adds a new item to the pouch contents.
+        item: object to be added
+        '''
+        self.pouch_contents.append(item)
+
+
+if __name__ == '__main__':
+    kanga = Kangaroo('Kanga')
+    roo = Kangaroo('Roo')
+    kanga.put_in_pouch('wallet')
+    kanga.put_in_pouch('car keys')
+    kanga.put_in_pouch(roo)
+
+    print(kanga)
