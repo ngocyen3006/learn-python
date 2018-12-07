8cfbaa2391e84696ec63363aff6c447463a29c73
Yen Nguyen <ngocyen300693@gmail.com>
2018-12-07 14:03:48 +0700

    Read file 'dang_3_phoi_hop_a.json'

--- learn_JSON/doc_dang_3a.py
+++ learn_JSON/doc_dang_3a.py
@@ -1,39 +1,5 @@
 import json
-
-
-def in_theo_cot(sinh_vien):
-    chuoi = sinh_vien["Ma_so"].ljust(10) + \
-            sinh_vien["Ho_ten"].ljust(30) + \
-            ngay(sinh_vien["Ngay_sinh"]).center(15) + \
-            gt(sinh_vien["Gioi_tinh"]).ljust(10) + \
-            so_tien(sinh_vien["Hoc_bong"]).rjust(10)
-    print(chuoi)
-
-
-def gt(gioi_tinh):
-    s = "Nam"
-    if gioi_tinh == 'false':
-        s = "Nu"
-    return s
-
-
-def so_tien(so):
-    return "{:,.0f} VND".format(so)
-
-
-def ngay(ngay_sinh):
-    s = ngay_sinh.split("-")
-    return s[2] + "/" + s[1] + "/" + s[0]
-
-
-def in_tieu_de():
-    chuoi = "Ma_so".ljust(10) + \
-            "Ho_ten".ljust(30) + \
-            "Ngay_sinh".center(15) + \
-            "Gioi_tinh".ljust(10) + \
-            "Hoc_bong".rjust(10)
-    print(chuoi)
-
+from ham_dinh_dang_nhan_vien import in_tieu_de, in_theo_cot
 
 f = open("dang_3_phoi_hop_a.json", "r", encoding="utf-8")
 du_lieu = json.load(f)
