efec3bc0a35aaf2b13d0abe8ff6227a6cd466552
Yen Nguyen <ngocyen300693@gmail.com>
2018-12-20 23:29:49 +0700

    Solve "Incorrect Regex" on hackerrank

---
+++ hackerrank/incorrectRegex.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/incorrect-regex/problem
+
+import re
+
+n = int(input())
+
+for i in range(n):
+    try:
+        re.compile(input())
+        print(True)
+    except Exception:
+        print(False)
