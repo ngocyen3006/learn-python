4d3eefc7a959083c23c21a94eeebc061308f42bf
Yen Nguyen <ngocyen300693@gmail.com>
2019-02-28 14:27:58 +0700

    Fix bug "Flip it!" on firecode.io

--- firecode.io/Level_01/flip_vertical_axis.py
+++ firecode.io/Level_01/flip_vertical_axis.py
@@ -1,14 +1,13 @@
 # Flip it!
 
 def flip_vertical_axis(matrix):
-    res = [0] * len(matrix)
     for i in range(len(matrix)):
         row = len(matrix[i])
         arr = [0] * row
         for j in range(row - 1, -1, -1):
             arr[row - j - 1] = matrix[i][j]
-        res[i] = arr
-    return res
+        matrix[i] = arr
+    return matrix
 
 
 if __name__ == '__main__':
@@ -17,3 +16,4 @@ if __name__ == '__main__':
            [[1, 2, 3], [4, 5, 6], [7, 8, 9]]]
     for i in arr:
         print(flip_vertical_axis(i))
+        print("-----------")
