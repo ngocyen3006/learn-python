297957ecc4ba886d8d8e4958b5d1007dc416dad8
Yen Nguyen <ngocyen300693@gmail.com>
2019-04-12 15:52:20 +0700

    Improve "fibonacci" exercise

--- leetcode.com/fibonacci.py
+++ leetcode.com/fibonacci.py
@@ -1,7 +1,10 @@
 # 509. Fibonacci Number
 # https://leetcode.com/problems/fibonacci-number/
 
-def fib(N):
+import math
+
+
+def fib2(N):
     f0 = -1
     f1 = 1
     f = 0
@@ -10,3 +13,10 @@ def fib(N):
         f0 = f1
         f1 = f
     return f
+
+
+def fib(N):
+    if N < 2:
+        return N
+    f = (1 / math.sqrt(5)) * ((((1 + math.sqrt(5)) / 2) ** N) - (((1 - math.sqrt(5) / 2) ** N)))
+    return round(f)
