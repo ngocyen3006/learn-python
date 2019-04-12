9d31caf593c3d7e9ce294c751f3cacf59f78060a
Yen Nguyen <ngocyen300693@gmail.com>
2019-04-12 11:45:31 +0700

    Improve "Fibonacci number"

--- leetcode.com/fibonacci.py
+++ leetcode.com/fibonacci.py
@@ -1,6 +1,12 @@
+# 509. Fibonacci Number
 # https://leetcode.com/problems/fibonacci-number/
 
 def fib(N):
-    if N < 2:
-        return N
-    return fib(N - 1) + fib(N - 2)
+    f0 = -1
+    f1 = 1
+    f = 0
+    for i in range(N + 1):
+        f = f0 + f1
+        f0 = f1
+        f1 = f
+    return f
