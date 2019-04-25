6936077e02cd54bd72579a22740b227f4fbf1637
Yen Nguyen <ngocyen300693@gmail.com>
2019-04-25 13:01:45 +0700

    Improve code '29. Divide Two Integers'

--- leetcode.com/divide.py
+++ leetcode.com/divide.py
@@ -5,12 +5,7 @@ def divide(dividend, divisor):
     if divisor == 0:
         raise ZeroDivisionError
 
-    d = 1
-    s = 1
-    if dividend < 0:
-        d = -1
-    if divisor < 0:
-        s = -1
+    positive = (dividend < 0) is (divisor < 0)
 
     dividend = abs(dividend)
     divisor = abs(divisor)
@@ -25,12 +20,8 @@ def divide(dividend, divisor):
             i <<= 1
             temp <<= 1
 
-    if d == -1 and s == 1 or s == -1 and d == 1:
+    if not positive:
         count = -count
 
     return min(max(-2147483648, count), 2147483647)
 
-
-dvd = 7
-dvs = 3
-print(divide(dvd, dvs))
