d17c1454de2a5a188dbb58bad1ca61bda7ad2507
Yen Nguyen <ngocyen300693@gmail.com>
2019-04-25 10:08:01 +0700

    Solve '58. Length of Last Word'

---
+++ leetcode.com/lengthOfLastWord.py
@@ -0,0 +1,12 @@
+# 58. Length of Last Word
+# https://leetcode.com/problems/length-of-last-word/
+
+def lengthOfLastWord(s):
+    if not s:
+        return 0
+    s = s.strip()
+    return len(s.split(' ')[-1])
+
+
+i = '  '
+print(lengthOfLastWord(i))
